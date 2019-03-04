48c4cc739ad43c4c68776aa5daa0311fd570ef00
Simone Locci <simone.locci@cloudesire.com>
2019-03-04 15:30:52 +0100

    Add cors header to simulate 504

--- simulate_504_n_times.py
+++ simulate_504_n_times.py
@@ -2,6 +2,7 @@ from mitmproxy import http, ctx
 
 URL_TO_INTERCEPT = 'http://example.com/path'
 MAX_NUMBER_OF_FAILS = 3
+CORS_HEADER = 'http://localhost:3000'
 
 
 class Simulate504:
@@ -9,11 +10,13 @@ class Simulate504:
         self.num = 0
 
     def request(self, flow: http.HTTPFlow):
-        if flow.request.pretty_url == URL_TO_INTERCEPT:
+        if flow.request.pretty_url == URL_TO_INTERCEPT and flow.request.method != 'OPTIONS':
             self.num = self.num + 1
             ctx.log.warn('Intercepted attempt number {} of {}'.format(self.num, MAX_NUMBER_OF_FAILS))
             if self.num <= MAX_NUMBER_OF_FAILS:
                 flow.response = http.HTTPResponse.make(504, b'{}', {'Content-Type': 'application/json'})
+                if CORS_HEADER:
+                    flow.response.headers['Access-Control-Allow-Origin'] = CORS_HEADER
             else:
                 self.num = 0  # reset
 
